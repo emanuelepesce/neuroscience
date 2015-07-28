9904ef1de7cdcef7d81da355843d1295d7cac048
emanuelepesce.ep@gmail.com <-->
2015-07-28 11:42:42 +0200

    final

--- source/R/epidemics/analyzeResults.R
+++ source/R/epidemics/analyzeResults.R
@@ -35,6 +35,7 @@ if(interactive()){
   
   c.data <- read.csv(pathInC, header=F)
   p.data <- read.csv(pathInP, header=F)
+
   
   m_mean2 <- matrix(ncol = 2, nrow = 3)
   colnames(m_mean2) <- c("controls", "patients")
@@ -51,6 +52,18 @@ if(interactive()){
     m_var2[i,1] <- var(c.data[,i+1])
     m_var2[i,2] <- var(p.data[,i+1])
   }
+
+  sd2 <- matrix(ncol = 2, nrow = 3)
+  colnames(sd2) <- c("controls", "patients")
+  rownames(sd2) <- c("time", "strong", "weak")
+  for(i in 1:3){ # controls patients
+    sd2[i,1] <- sd(c.data[,i+1])
+    sd2[i,2] <- sd(p.data[,i+1])
+  } 
+  m2 <- t(as.matrix(sd2))
+  barplot(m2, col=c("darkblue","red"), legend = rownames(m1), ylim = c(0,100), beside = TRUE)
+  m2 <- t(as.matrix(m_mean2))
+  barplot(m2, col=c("darkblue","red"), legend = rownames(m1), ylim = c(0,400), beside = TRUE)
   
   m_mean2
   m_var2
@@ -88,5 +101,13 @@ if(interactive()){
   m_var2
   m_mean3
   m_var3
+
+  write.csv(sd2, file="./res/sd2.csv")
   
+#   write.csv(m_mean1, file="./res/m_mean1.csv")
+#   write.csv(m_mean2, file="./res/m_mean2.csv")
+#   write.csv(m_mean3, file="./res/m_mean3.csv")
+#   write.csv(m_var1, file="./res/m_var1.csv")
+#   write.csv(m_var2, file="./res/m_var2.csv")
+#   write.csv(m_var3, file="./res/m_var3.csv")
 }
